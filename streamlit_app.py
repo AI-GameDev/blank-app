59c8b2d1d9f8a878ac567529d01772978f960612
GameDev&AI <lovedajava@nate.com>
2024-10-03 04:10:16 +0000

    title 변경

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("👍 내가 만든 앱")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
